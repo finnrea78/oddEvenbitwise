7ef15dac0335da462036f80c58e278feb612eca5
Finn Rea <b9020748@newcastle.ac.uk>
2021-01-28 17:39:18 +0000

    Added

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 1;
+        int check = num&1;
+        if(check == 0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+
+
+
+
+        int x = 12, y = 7;
+        boolean z;
+        z = x != 4 || y == 2;
+        System.out.println("z = " + z);
+
+
+    }
+}
